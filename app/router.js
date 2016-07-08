0077d9735160c7e7e6282b330bb0313e98cc67db
Tyler Love <t@tylr.org>
2016-07-08 15:36:42 -0400

    refactor css

--- app/router.js
+++ app/router.js
@@ -6,6 +6,23 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('portfolio');
+  this.route('contact');
 });
+
+Ember.Route.reopen({
+  activate: function() {
+    const cssClass = this.toCssClass();
+    if (cssClass != 'application') {
+      Ember.$('body').addClass(cssClass);
+    }
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass(this.toCssClass());
+  },
+  toCssClass: function() {
+    return this.routeName.replace(/\./g, '-').dasherize();
+  }
+})
 
 export default Router;
